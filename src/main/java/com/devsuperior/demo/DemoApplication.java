687d666e3af3762fbcea0679630cbc61aa770941
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-08-18 20:38:37 -0300

    Implementacao exemplo de como funciona a codificacao, a criacao do hash para as senhas no BD pelo Bcrypt

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class DemoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+		
+		//Como o Spring compara a senha passada pelo usuario com o hash criado pelo Bcrypt:
+		boolean result = passwordEncoder.matches("123456", "$2a$10$aHRdy5X04TZBty2IPJxx5.t.cly/99ztZTcHe594Hd625VfbAHj4u");
+		
+		System.out.println(result);
+	}
+	
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$aHRdy5X04TZBty2IPJxx5.t.cly/99ztZTcHe594Hd625VfbAHj4u');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$aHRdy5X04TZBty2IPJxx5.t.cly/99ztZTcHe594Hd625VfbAHj4u');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
